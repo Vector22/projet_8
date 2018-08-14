ba44c5fb781b4b8a4a576d30d83cf9c1503aa4d2
v3ct0r22 <rekinvector@gmail.com>
2018-08-14 10:21:36 +0000

    Reprise de la methode pour enregistrer un aliment en favoris

--- pure_beurre/food/urls.py
+++ pure_beurre/food/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.login,
          {'template_name': 'food/registration/login.html'}, name='login'),
-    path('logout/', auth_views.logout,
-         {'template_name': 'food/registration/login.html'}, name='logout'),
+    path('logout/', views.log_out, name='logout'),
     path('result/<str:searchedFood>', views.result, name='result'),
+    #path('saveFood/', views.saveFood, name='saveFood'),
     path('all_foods/', views.all_foods, name='all_foods'),
     path('test/', views.test)
     ]
--- pure_beurre/food/views.py
+++ pure_beurre/food/views.py
@@ -1,6 +1,7 @@
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
+from django.urls import reverse
 from django.shortcuts import render, redirect
 import json
 
@@ -40,40 +41,6 @@ def result(request, searchedFood):
 
     return render(request, 'food/result.html', locals())
 
-@csrf_exempt
-def saveFood(request):
-    """ This function perform the task what permit
-    to save a food in favorite by the user
-    """
-
-    # the variables to return to the client side
-    alreadySaved = False
-    saved = False
-
-    # recover the data transmitted by the client
-    foodId = int(request.POST['foodId'])
-
-    userId = request.user.id  # the user's id
-    # verfy if the food has been already saved by the user
-    foodSaved = MyFood.objects.filter(userId=userId).exists()
-    # if never saved, save it
-    if not foodSaved:
-        alreadySaved = True
-        saved = True
-        food = Food.objects.get(id=foodId)  # the concerned food
-        # save the food in the db
-        favoriteFood = MyFood.objects.create()
-        favoriteFood.userId = userId
-        favoriteFood.food = food
-        favoriteFood.save()
-
-    return json.dumps({'alreadySaved': alreadySaved,
-                       'saved': saved})
-
-
-def test(request):
-    return render(request, 'food/test.html')
-
 
 def signup(request):
     if request.method == 'POST':
@@ -90,7 +57,16 @@ def signup(request):
     return render(request, 'food/registration/signup.html', {'form': form})
 
 
+def log_out(request):
+    logout(request)
+    return redirect(reverse(home))
+
+
 def all_foods(request):
     foods = Food.objects.all()
     total = Food.objects.count()
     return render(request, 'food/all_foods.html', locals())
+
+
+def test(request):
+    return render(request, 'food/test.html')
