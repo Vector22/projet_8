b94579e302ff6ce78aa5465b8030e13ff6c40928
v3ct0r22 <rekinvector@gmail.com>
2018-09-05 17:07:13 +0000

    Review the path / on heroku deploiement

--- pure_beurre/pure_beurre/settings.py
+++ pure_beurre/pure_beurre/settings.py
@@ -29,7 +29,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
 else:
     DEBUG = True
 
-ALLOWED_HOSTS = ['vector-pure-beurre.herokuapp.com']
+ALLOWED_HOSTS = ['vector-pure-beurre.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
