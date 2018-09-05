94d4dc36b58e7f2faa38b1d7b35103c8ac957d2b
v3ct0r22 <rekinvector@gmail.com>
2018-09-05 15:21:48 +0000

    review the configuration file

--- pure_beurre/pure_beurre/settings.py
+++ pure_beurre/pure_beurre/settings.py
@@ -29,7 +29,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
 else:
     DEBUG = True
 
-ALLOWED_HOSTS = ['pur-beurre.herokuapp.com']
+ALLOWED_HOSTS = ['pure_beurre.herokuapp.com']
 
 
 # Application definition
