4b87fe92a98d174fa2cc8391fdbe5fbc711053ba
Vector22 <rekinvector@gmail.com>
2021-01-18 17:38:18 +0000

    Update settings about statics files.

--- pure_beurre/pure_beurre/settings.py
+++ pure_beurre/pure_beurre/settings.py
@@ -145,12 +145,18 @@ MEDIA_URL = '/media/'
 if os.environ.get('ENV') == 'PRODUCTION':
 
     # Static files settings
-    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+    # PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+    # STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
     # Extra places for collectstatic to find static files.
-    STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'), )
+    # STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'), )
+
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_URL = '/static/'
+
+    # Extra places for collectstatic to find static files.
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
     # Simplified static file serving.
     # https://warehouse.python.org/project/whitenoise/
